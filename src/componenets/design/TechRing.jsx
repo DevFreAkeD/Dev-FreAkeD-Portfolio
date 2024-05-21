fc0022e67528d1afb9baaee5fac1b667b10c3622
Tushar Nagar <tusharrock146@gmail.com>
2024-05-21 15:14:55 +0530

    Create TechRing.jsx

---
+++ src/componenets/design/TechRing.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { stack } from "../../assets";
+
+const TechRing = ({ apps }) => {
+  return (
+    <div className="relative flex w-[22rem] aspect-square border border-n-6 rounded-full scale-75 md:scale-100">
+      <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+        <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+          <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+              <img
+                src={stack}
+                width={48}
+                height={48}
+                  alt="stack"
+              />
+          </div>
+        </div>
+      </div>
+
+      <ul className="absolute inset-0 flex justify-center items-center rotate-icons">
+        {apps.map((app, index) => (
+          <li
+            key={app.id}
+            className="absolute flex justify-center items-center"
+            style={{ transform: `rotate(${index * 45}deg) translateX(11rem) rotate(-${index * 45}deg)` }}
+          >
+            <div className="relative flex justify-center items-center w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl">
+              <img
+                className="m-auto"
+                width={app.width}
+                height={app.height}
+                alt={app.title}
+                src={app.icon}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TechRing;
